d9805659312fd05f22c7e748f399fdf84d34c338
pablowaller <76492840+pablowaller@users.noreply.github.com>
2023-11-03 22:56:16 -0300

    Create Encoder.ino

---
+++ Encoder.ino
@@ -0,0 +1,59 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+#define ENCODER_CLK 2
+#define ENCODER_DT  3
+#define ENCODER_SW  4
+
+int counter = 0;
+
+void setup() {
+  // Initialize LCD
+  lcd.init();
+  lcd.backlight();
+
+  // Initialize encoder pins
+  pinMode(ENCODER_CLK, INPUT);
+  pinMode(ENCODER_DT, INPUT);
+  pinMode(ENCODER_SW, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENCODER_CLK), readEncoder, FALLING);
+}
+
+void readEncoder() {
+  int dtValue = digitalRead(ENCODER_DT);
+  if (dtValue == HIGH) {
+    counter++; // Clockwise
+  }
+  if (dtValue == LOW) {
+    counter--; // Counterclockwise
+  }
+}
+
+// Get the counter value, disabling interrupts.
+// This make sure readEncoder() doesn't change the value
+// while we're reading it.
+int getCounter() {
+  int result;
+  noInterrupts();
+  result = counter;
+  interrupts();
+  return result;
+}
+
+void resetCounter() {
+  noInterrupts();
+  counter = 0;
+  interrupts();
+}
+
+void loop() {
+  lcd.setCursor(3, 0);
+  lcd.print("Counter:");
+  lcd.setCursor(7, 1);
+  lcd.print(getCounter());
+  lcd.print("        ");
+
+  if (digitalRead(ENCODER_SW) == LOW) {
+    resetCounter();
+  }
+}
