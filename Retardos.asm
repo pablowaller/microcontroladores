92bfab8504c52a35406b5fec96015ad927f35736
pablowaller <76492840+pablowaller@users.noreply.github.com>
2025-07-15 21:02:32 -0300

    Update Retardos.asm

--- Retardos.asm
+++ Retardos.asm
@@ -1,20 +1,25 @@
-
 retardo_XXX_mseg
- movlw d'5';
- movwf CONTADOR_3;
+    movlw d'5'
+    movwf CONTADOR_3       ; Bucle externo, 5 repeticiones
+
 loop_2
- movlw d'100';
- mowf CONTADOR_2;
+    movlw d'100'
+    movwf CONTADOR_2       ; Segundo nivel, 100 repeticiones
+
 loop_1
- movlw d'250';
- movwf CONTADOR_1;
+    movlw d'250'
+    movwf CONTADOR_1       ; Bucle interno, 250 repeticiones
+
 loop_k
- NOP; este nop me permite tener un retardo mas preciso
- decfsz CONTADOR_1;
- GOTO loop_k; un retardo de __us
- decfsz CONTADOR_2;
- GOTO loop_1; un retardo de ___ms
- decfsz CONTADOR_3;
- GOTO loop_2; un retardo de ___s
-nop;
-return
+    NOP                    ; Este NOP da precisión (1 μs)
+    decfsz CONTADOR_1, f
+    GOTO loop_k            ; 250 * (2+1) μs = ~750 μs aprox
+
+    decfsz CONTADOR_2, f
+    GOTO loop_1            ; 100 * 750 μs = ~75 ms
+
+    decfsz CONTADOR_3, f
+    GOTO loop_2            ; 5 * 75 ms = **375 ms total aprox**
+
+    NOP
+    RETURN
