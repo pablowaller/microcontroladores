7328d5e4d4c2e1608a2cfc7e325fa29c538ceca3
pablowaller <76492840+pablowaller@users.noreply.github.com>
2025-07-15 21:13:33 -0300

    Update Subrutina.asm

--- Subrutina.asm
+++ Subrutina.asm
@@ -1,27 +1,40 @@
-main
+            ORG 0x000
+            GOTO main
 
+; ========================
+CONTADOR_1  EQU 0x20
+CONTADOR_2  EQU 0x21
+CONTADOR_3  EQU 0x22
+; ========================
+
+main
+            call retardo_XXX_mseg
+            goto main
+
+; ========================
 retardo_XXX_mseg
-  movlw d'5';
-  movwf CONTADOR_3;
+            movlw d'5'
+            movwf CONTADOR_3      ; bucle externo (multiplica duración)
 
 loop_2
-  movlw d'100';
-  movwf CONTADOR_2;
+            movlw d'100'
+            movwf CONTADOR_2      ; bucle medio
 
 loop_1
- movlw d'250';
- movwf CONTADOR_1;
+            movlw d'250'
+            movwf CONTADOR_1      ; bucle interno
 
 loop_k
-  NOP;
-  decfsz CONTADOR_1;
-  GOTO loop_k; 
-  decfsz CONTADOR_2;
-  GOTO loop_1;
-  decfsz CONTADOR_3;
-  GOTO loop_2;
+            NOP
+            decfsz CONTADOR_1, f
+            goto loop_k
 
-  nop
-  return
+            decfsz CONTADOR_2, f
+            goto loop_1
 
-	END     
+            decfsz CONTADOR_3, f
+            goto loop_2
+
+            return
+
+            END
